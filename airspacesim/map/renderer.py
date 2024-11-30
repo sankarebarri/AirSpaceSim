366f2e2cc2b22d52b87bb53fafbd2ab0ad855298
sankarebarri <sankarebarri_dev@yahoo.com>
2024-11-30 20:41:10 +0000

    codes refactored and optimised

--- airspacesim/map/renderer.py
+++ airspacesim/map/renderer.py
@@ -1,11 +1,12 @@
 import json
-class MapRenderer:
-    def __init__(self, center=None, Zoom=10):
-        """
-        Initialise the map with a center and a zoom level.
 
-        :param center: List of [latitude, longitude].
-        :param: zoom: Integer zoom level. Default to 10.
+class MapRenderer:
+    def __init__(self, center=None, zoom=10):
+        """
+        Initialize the map with a center and zoom level.
+
+        :param center: List of [latitude, longitude] for the map center. Default: [0, 0].
+        :param zoom: Integer zoom level. Default: 10.
         """
         self.config = {
             "center": center or [0, 0],
@@ -14,11 +15,19 @@ class MapRenderer:
             "elements": []
         }
 
+    def add_element(self, element_type, **kwargs):
+        """
+        Generalized method to add elements to the map.
+        :param element_type: Type of the element ('circle', 'polyline', 'marker').
+        """
+        element = {"type": element_type, **kwargs}
+        self.config["elements"].append(element)
+
     def add_tile_layer(self, url=None, attribution=None):
         """
         Add a tile layer to the map. Default is OpenStreetMap.
-        
-        :param url: Tile layer URL template. Default to OpenStreetMap.
+
+        :param url: Tile layer URL template. Default: OpenStreetMap.
         :param attribution: Attribution for the map tiles.
         """
         self.config["tile_layer"] = {
@@ -26,64 +35,17 @@ class MapRenderer:
             "attribution": attribution or "© OpenStreetMap contributors"
         }
 
-    def add_circle_boundary(self, center, radius, **kwargs):
-        """
-        Add a circle boundary (airspace boundary).
-
-        :param center: [latitude, longitude].
-        :param radius: Radius in meters.
-        """
-        element = {"type": "circle", "center": center, "radius": radius}
-        element.update(kwargs)
-        self.config["elements"].append(element)
-
-    def add_polyline(self, coords, **kwargs):
-        """
-        Add a polyline (route).
-
-        :param coords: List of [latitude, longitude].
-        """
-        element = {"type": "polyline", "coords": coords}
-        element.update(kwargs)
-        self.config["elements"].append(element)
-
-    def add_marker(self, coords, **kwargs):
-        """
-        Add a marker (waypoint).
-
-        :param coords: [latitude, longitude]
-        """
-        element = {"type": "marker", "coords": coords}
-        element.update(kwargs)
-        #Ensure label_text is passed for permanent wayppoint labels
-        if label_text not in element and popup_text in element:
-            element["label_text"] = element["popup_text"]
-        self.config["elements"].append(element)
-
-    def update_element(self, element_type, element_id, **updates):
-        """
-        Update an existing map element.
-
-        :param element_type: Type of element ("circle", "polyline", "marker").
-        :param element_id: Index of the element in the elements list.
-        :param updates: Fields to update in the element.
-        """
-        elements = [e for e in self.config["elements"] if e["type"] == element_type]
-        if element_id < 0 or element_id >= len(elements):
-            raise IndexError(f"No {element_type} with ID {element_id}")
-        elements[element_id].update(updates)
-
     def export_config(self):
         """
         Export the map configuration as a dictionary.
 
-        :return: Mapp configuration.
+        :return: Map configuration.
         """
         return self.config
 
     def to_json(self, filepath="map_config.json"):
         """
-        Export the map configuartion to a JSON file.
+        Export the map configuration to a JSON file.
 
         :param filepath: Path to save the JSON file.
         """
--- airspacesim/routes/manager.py
+++ airspacesim/routes/manager.py
@@ -1,34 +1,28 @@
 from airspacesim.utils.conversions import dms_to_decimal
 
+
 class RouteManager:
     def __init__(self, name):
         """
-        Initialise a route with a name
+        Initialize a route with a name.
 
-        :param name: Name of the route
+        :param name: Name of the route.
         """
         self.name = name
-        self.waypoints = [] # List of waypoints, each as [lat, lon, name]
+        self.waypoints = []
 
     def add_waypoint(self, coords, name=None, distance=None, altitude=None):
         """
-        Add a waypoint to the route
+        Add a waypoint to the route.
 
         :param coords: {"lat": (degrees, minutes, seconds, direction), "lon": (degrees, minutes, seconds, direction)}.
         :param name: Optional name for the waypoint.
         :param distance: Optional distance to the next waypoint.
-        :param altitude: Optiona altitude for the waypoint
+        :param altitude: Optional altitude for the waypoint.
         """
-        # Ensure coords is in the correct format
         if not isinstance(coords, dict) or "lat" not in coords or "lon" not in coords:
-            raise ValueError(
-                f"Invalid coordinates format: {coords}. Expected a dictionary with a 'lat' and 'lon' keys."
-            )
-        # Convert DMS coords to decimal coords. Needed by leaflet.js
-        dec_coords = [
-            dms_to_decimal(*coords["lat"]),
-            dms_to_decimal(*coords["lon"])
-        ]
+            raise ValueError("Invalid coordinates format. Expected dict with 'lat' and 'lon'.")
+        dec_coords = [dms_to_decimal(*coords["lat"]), dms_to_decimal(*coords["lon"])]
         self.waypoints.append({
             "coords": coords,
             "dec_coords": dec_coords,
@@ -37,17 +31,6 @@ class RouteManager:
             "altitude": altitude
         })
 
-    def remove_waypoint(self, index):
-        """
-        Remove a waypoint by its index.
-
-        :param index: Index of the waypoint to remove.
-        """
-        if 0 <= index < len(self.waypoints):
-            del waypoints["index"]
-        else:
-            raise IndexError("Invalid waypoint index.")
-
     def get_waypoints(self):
         """
         Get all waypoints in the route.
@@ -55,19 +38,3 @@ class RouteManager:
         :return: List of waypoints as dictionaries.
         """
         return self.waypoints
-
-    def visualise_on(self, map_renderer):
-        """
-        Visualise the route on a map.__annotations__
-        
-        :param map_renderer: Instance of MapRenderer to render the route.
-        """
-        coords = [wp["dec_coords"] for wp in self.waypoints]
-        map_renderer.add_polyline(coords, color="red", name=self.name)
-        for wp in self.waypoints:
-            popup_text = f"Waypoint {wp['name'] or 'Unnamed'}<br>"
-            if wp.get("distance"):
-                popup_text += f"Distance to next: {wp['distance']} NM <br>"
-            if wp.get("altitude"):
-                popup_text += f"Altitude: {wp['altitude']} ft"
-            map_renderer.add_marker(wp["coords"], popup_text=wp["name"])
