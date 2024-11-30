781118281ee40279d56899dc22db57c541306d09
sankarebarri <sankarebarri_dev@yahoo.com>
2024-11-30 14:17:11 +0000

    rendere.py -> MapRenderer rewritten so it can't export the map configuration as a JSON file

--- airspacesim/map/renderer.py
+++ airspacesim/map/renderer.py
@@ -1,12 +1,10 @@
-import json
-
 class MapRenderer:
-    def __init__(self, center=None, zoom=10):
+    def __init__(self, center=None, Zoom=10):
         """
-        Initialize the map with a center and zoom level.
+        Initialise the map with a center and a zoom level.
 
-        :param center: List of [latitude, longitude] for the map center. Default: [0, 0].
-        :param zoom: Integer zoom level. Default: 10.
+        :param center: List of [latitude, longitude].
+        :param: zoom: Integer zoom level. Default to 10.
         """
         self.config = {
             "center": center or [0, 0],
@@ -18,8 +16,8 @@ class MapRenderer:
     def add_tile_layer(self, url=None, attribution=None):
         """
         Add a tile layer to the map. Default is OpenStreetMap.
-
-        :param url: Tile layer URL template. Default: OpenStreetMap.
+        
+        :param url: Tile layer URL template. Default to OpenStreetMap.
         :param attribution: Attribution for the map tiles.
         """
         self.config["tile_layer"] = {
@@ -29,7 +27,7 @@ class MapRenderer:
 
     def add_circle_boundary(self, center, radius, **kwargs):
         """
-        Add a circle boundary (airspace).
+        Add a circle boundary (airspace boundary).
 
         :param center: [latitude, longitude].
         :param radius: Radius in meters.
@@ -42,7 +40,7 @@ class MapRenderer:
         """
         Add a polyline (route).
 
-        :param coords: List of [latitude, longitude] pairs.
+        :param coords: List of [latitude, longitude].
         """
         element = {"type": "polyline", "coords": coords}
         element.update(kwargs)
@@ -50,19 +48,22 @@ class MapRenderer:
 
     def add_marker(self, coords, **kwargs):
         """
-        Add a marker (e.g., waypoint).
+        Add a marker (waypoint).
 
-        :param coords: [latitude, longitude].
+        :param coords: [latitude, longitude]
         """
         element = {"type": "marker", "coords": coords}
         element.update(kwargs)
+        #Ensure label_text is passed for permanent wayppoint labels
+        if label_text not in element and popup_text in element:
+            element["label_text"] = element["popup_text"]
         self.config["elements"].append(element)
 
     def update_element(self, element_type, element_id, **updates):
         """
         Update an existing map element.
 
-        :param element_type: Type of element ('circle', 'polyline', 'marker').
+        :param element_type: Type of element ("circle", "polyline", "marker").
         :param element_id: Index of the element in the elements list.
         :param updates: Fields to update in the element.
         """
@@ -75,13 +76,13 @@ class MapRenderer:
         """
         Export the map configuration as a dictionary.
 
-        :return: Map configuration.
+        :return: Mapp configuration.
         """
         return self.config
 
     def to_json(self, filepath="map_config.json"):
         """
-        Export the map configuration to a JSON file.
+        Export the map configuartion to a JSON file.
 
         :param filepath: Path to save the JSON file.
         """
